68f917f0ba0fe336619c5d6218b49fcd94cbc544
gabrovski <sashogabrovski@gmail.com>
2012-03-22 04:31:40 -0400

    cleaned the db updater and made it handle multiple pickles since theres only 1.3 GB of RAM on hacktown

--- _data/update_db.py
+++ _data/update_db.py
@@ -1,39 +1,8 @@
-import sqlite3, cPickle, re, time, psycopg2
+import sqlite3, cPickle, re, time, psycopg2, os
 
 def get_conn():
-    #c = sqlite3.connect('../db.sqlite')
     c = psycopg2.connect('dbname=knowevo user=knowevo host=localhost password=12345c7890')
-    #c.text_factory = str
     return c
-
-def format_name(name):
-    name = re.sub('[\(\)]' ,'', name)
-    name = re.sub('[ -]', '_', name)
-    return name
-
-def insert_articles(path, ed):
-    f = open(path)
-    arts = cPickle.load(f)
-    f.close()
-
-    conn = get_conn()
-    c = conn.cursor()
-
-    for a in arts:
-        if 'id' not in a:
-            print 'id not found'
-            break
-        
-        name, art_id, art_ed, text = a['name'], a['id'], ed, a['txt']
-        if ed == 15:
-            text = ''
-        print name
-
-        c.execute('insert into incunabula_article\n'+
-                  'values(null,%s,%d,%d,%s)', (name, art_id, art_ed, text))
-
-    conn.commit()
-    c.close()
 
 def clean_db():
     conn = get_conn()
@@ -43,6 +12,8 @@ def clean_db():
     c.execute('DELETE FROM incunabula_masterarticle')
     conn.commit()
     c.close()
+    
+    print 'cleaned db'
 
 def insert_master_list(revw):
     conn = get_conn()
@@ -53,6 +24,8 @@ def insert_master_list(revw):
                   '(name) values(%s)', (k,))
     conn.commit()
     c.close()
+
+    print 'inserted master articles'
 
 def insert_matching_articles(revw):
     conn = get_conn()
@@ -81,6 +54,8 @@ def insert_matching_articles(revw):
             
     conn.commit()
     c.close()
+    
+    print 'inserted articles and matches'
 
 
 def load(path):
@@ -89,10 +64,16 @@ def load(path):
     f.close()
     return revw
 
-if __name__ == '__main__':
+def process_split():
     clean_db()
-    revw = load('sample_revw.pkl')
-    insert_master_list(revw)
-    insert_matching_articles(revw)
     
+    for name in os.listdir('split'):
+        revw = load('split/'+name)
+        insert_master_list(revw)
+        insert_matching_articles(revw)
+    
+    
+
+if __name__ == '__main__':
+    process_split()
                   
