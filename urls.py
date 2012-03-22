411ba390289080aa3b64bf90abdfb5219212c4b2
Sasho <gabrovski@thinkpad.(none)>
2012-03-22 17:23:27 -0400

    modified urls so that it works for deployment and localhost both

--- urls.py
+++ urls.py
@@ -3,14 +3,32 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'incunabula.views.index'),
-    url(r'^(?P<article_id>\d+)/$', 'incunabula.views.article_detail'),
-    url(r'^(?P<master_name>.+?)/$', 'incunabula.views.master_detail'),
+USER_LOCALHOST = True
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+if USER_LOCALHOST:
+        urlpatterns = patterns('',
+                           url(r'^$', 'incunabula.views.index'),
+                           url(r'^incunabula/$', 'incunabula.views.index'),
+                           url(r'^incunabula/(?P<article_id>\d+)/$', 'incunabula.views.article_detail'),
+                           url(r'^incunabula/(?P<master_name>.+?)/$', 'incunabula.views.master_detail'),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                           # Uncomment the admin/doc line below to enable admin documentation:
+                           url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                           # Uncomment the next line to enable the admin:
+                           url(r'^admin/', include(admin.site.urls)),
+)
+
+#for Apache deployment
+else:
+    urlpatterns = patterns('',
+                           url(r'^$', 'incunabula.views.index'),
+                           url(r'^(?P<article_id>\d+)/$', 'incunabula.views.article_detail'),
+                           url(r'^(?P<master_name>.+?)/$', 'incunabula.views.master_detail'),
+
+                           # Uncomment the admin/doc line below to enable admin documentation:
+                           url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                           # Uncomment the next line to enable the admin:
+                           url(r'^admin/', include(admin.site.urls)),
 )
