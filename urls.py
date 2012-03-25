b429ec80f2e8449996b9d9aace0287dde85cae7e
Sasho <gabrovski@thinkpad.(none)>
2012-03-25 02:45:08 -0400

    reformatted, emacs can be really stupid sometimes

--- urls.py
+++ urls.py
@@ -6,17 +6,15 @@ admin.autodiscover()
 USER_LOCALHOST = True
 
 if USER_LOCALHOST:
-        urlpatterns = patterns('',
-                           url(r'^$', 'incunabula.views.index'),
-                           url(r'^incunabula/$', 'incunabula.views.index'),
-                           url(r'^incunabula/(?P<article_id>\d+)/$', 'incunabula.views.article_detail'),
-                           url(r'^incunabula/(?P<master_name>.+?)/$', 'incunabula.views.master_detail'),
-
-                           # Uncomment the admin/doc line below to enable admin documentation:
-                           url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                           # Uncomment the next line to enable the admin:
-                           url(r'^admin/', include(admin.site.urls)),
+	urlpatterns = patterns('',
+			       url(r'^$', 'incunabula.views.index'),
+			       url(r'^incunabula/$', 'incunabula.views.index'),
+			       url(r'^incunabula/(?P<article_id>\d+)/$', 'incunabula.views.article_detail'),
+			       url(r'^incunabula/(?P<master_name>.+?)/$', 'incunabula.views.master_detail'),
+			   
+			       # Uncomment the admin/doc line below to enable admin documentation
+			       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+			       url(r'^admin/', include(admin.site.urls)),
 )
 
 #for Apache deployment
