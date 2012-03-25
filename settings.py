e2d3f501224d789671acccfdd2b1f2b9cde847d0
gabrovski <sashogabrovski@gmail.com>
2012-03-25 05:01:22 -0400

    deployed

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'knowevo.urls'
 
 TEMPLATE_DIRS = (
-    '/home/gabrovski/cs/knowevo/templates/',
+    '/home/sasho/cs/knowevo/templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
