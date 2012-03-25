5faf725c8b88389dfe701fcab1324de33a3d25b7
Sasho <gabrovski@thinkpad.(none)>
2012-03-25 01:05:21 -0400

    created gravebook models

--- gravebook/models.py
+++ gravebook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name     = models.CharField(max_length=256, primary_key=True)
+
+class Article(models.Model):
+    name  = models.CharField(max_length=256, primary_key=True)
+    wid   = models.BigIntegerField()
+    birth = models.IntegerField()
+    death = models.IntegerField()
+    
+    influences = models.ManyToManyField('self')
+    influenced = models.ManyToManyField('self')
+    peers      = models.ManyToManyField('self')
+
+    categories = models.ManyToManyField(Category)
+
