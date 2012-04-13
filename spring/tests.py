297fc80bf18646e6df6a8f531c7c4db7b55138ce
sasho <sashogabrovski@gmail.com>
2012-04-12 20:31:18 -0400

    prepared test

--- spring/tests.py
+++ spring/tests.py
@@ -6,11 +6,56 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from spring.force import SpringBox
+
+class SpringTestNoGraphics(TestCase):
+    class Holder():
+        def __init__(self, name):
+            self.name = name
+
+    @staticmethod
+    def connect_all(ndict, objects, w):
+        for i in xrange(len(objects)):
+            for j in xrange(i+1, len(objects)):
+                vn = objects[i].name
+                un = objects[j].name
+                
+                if vn not in ndict:
+                    ndict[vn] = dict()
+                ndict[vn][un] = w
+
+                if un not in ndict:
+                    ndict[un] = dict()
+                ndict[un][vn] = w
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    @staticmethod
+    def kfn(ndict, u, v):
+        if u.name not in ndict or v.name not in ndict[u.name]:
+            return None
+        return ndict[u.name][v.name]
+
+    @staticmethod
+    def test_draw_sb(sb):
+        pass
+
+    def test_spring(self):
+        #self.assertEqual(1 + 1, 2)
+        AB = 'abcdefghijklmnopqrstuvwxyz'.upper()
+        objects = []
+        for c in AB:
+            objects.append(self.Holder(c))
+
+        ndict = dict()
+        SpringTestNoGraphics.connect_all(ndict, objects[:10],   1)
+        SpringTestNoGraphics.connect_all(ndict, objects[10:20], 2)
+        SpringTestNoGraphics.connect_all(ndict, objects[20:],   3)
+        
+        sb = SpringBox(objects=objects, width=150, height=400, 
+                       charge=1, mass=1, time_step=0.05, 
+                       kfn=lambda x, y: SpringTestNoGraphics.kfn(ndict, x, y))
+
+        sb.move_to_equillibrium(1000)
+        SpringTestNoGraphics.test_draw_sb(sb)
+        
+        
