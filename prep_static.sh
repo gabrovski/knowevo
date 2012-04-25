8e74fd8ce6730eef616bd83e21926bf9be880f41
gabrovski <sashogabrovski@gmail.com>
2012-04-25 00:29:18 -0400

    created a simple deployment helper

---
+++ prep_static.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "obtaining gephi" ;
+cd static ;
+wget https://launchpad.net/gephi/toolkit/toolkit-0.8.5/+download/gephi-toolkit-0.8.5-all.zip ;
+unzip gephi-toolkit-0.8.5-all.zip ;
+cp gephi-toolkit-0.8.1-all/gephi-toolkit.jar . ;
+
+echo "gephi's there. use netbeans to add the jar in static to the GraphDrawerProject and build it using ant jar. start the server by running GraphDrawer's jar file" ;
+
+echo "getting js files" ;
+mkdir js ;
+wget -P js http://code.jquery.com/jquery-1.7.2.min.js ;
+wget -P js http://code.jquery.com/jquery-1.7.2.js ;
+
+echo "adding necessary categories" ;
+mkdir stroot ;
+mkdir pngs ;
+
+cd .. ;
+echo "ensure that you have the proper python loaded "
+echo "collecting static files into stroot. make sure that settings is configure properly in terms of absolute paths " ;
+python manage.py collectstatic ;
+
