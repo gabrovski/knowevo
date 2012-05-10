6c9404d7f280b3a6e67c7e19c2635eefe179ba02
gabrovski <sashogabrovski@gmail.com>
2012-05-10 05:48:40 -0400

    hacktown doesnt serve usr files. moved to caligari

--- prep_static.sh
+++ prep_static.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 echo "getting jars" ;
-wget http://hacktown.cs.dartmouth.edu/~sasho/libs.tar.gz ;
+wget http://caligari.dartmouth.edu/~agabrovs/libs.tar.gz ;
 tar xfvz libs.tar.gz ;
 mv lib jar/lib ;
 
 echo "getting required js files" ;
-wget http://hacktown.cs.dartmouth.edu/~sasho/js.tar.gz ;
+wget http://caligari.dartmouth.edu/~agabrovs/js.tar.gz ;
 tar xfvz js.tar.gz ;
 mv js static/js ;
-
-echo "ensure that you have the proper python loaded "
-echo "collecting static files into stroot. make sure that settings is configure properly in terms of absolute paths " ;
-python manage.py collectstatic ;
 
