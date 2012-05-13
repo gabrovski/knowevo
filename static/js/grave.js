8baabb9901a0c44cfc0700413c4a871bb40fe549
sasho <sashogabrovski@gmail.com>
2012-05-13 19:18:54 -0400

    created ajax form submission. took forever to figure out

---
+++ static/js/grave.js
@@ -0,0 +1,36 @@
+
+function inputFocus(i) {
+    if (i.value == i.defaultValue) {
+	i.value = "";
+	i.style.color = "#000";
+    }
+}
+
+function inputBlur(i) {
+    if (i.value == "") {
+	i.value = i.defaultValue;
+	i.style.color = "#888";
+    }
+}
+
+function searchSubmit(form) {
+    var inp = form.title_inp.value;
+    //$('#res_container').load(inp);
+    var dataString = 'title_inp='+inp;
+    try {
+    $.ajax({  
+	type: "POST",  
+	url: "index/",  
+	data: dataString,  
+	headers: {'X-CSRFToken': csrf_token},
+	success: function() {  
+	    $('#res_container').html("dwdwadwawa");  
+	}
+    });
+    }
+    catch (err) {
+	alert(err.message);
+    }
+}
+	
+
