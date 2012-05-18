05d25772334e1ab6ee724b56c8f61d6696ee9b57
sasho <sashogabrovski@gmail.com>
2012-05-18 00:51:28 -0400

    gravebook model adapted to include britannica articles

--- gravebook/models.py
+++ gravebook/models.py
@@ -16,7 +16,7 @@ class Article(models.Model):
     wid   = models.BigIntegerField()
     birth = models.IntegerField(blank=True, null=True)
     death = models.IntegerField(blank=True, null=True)
-    image = models.CharField(max_length=512)
+    image = models.CharField(max_length=512, default='null')
     
     people = models.ManyToManyField('self')
     peers  = models.ManyToManyField('self')    
@@ -27,6 +27,11 @@ class Article(models.Model):
     linked_by = models.ManyToManyField('self')
     
     vscore = models.FloatField(default=0.0)
+
+    #100 means wikipedia. hacky slashy
+    art_ed = models.IntegerField(default=1000)
+    text = models.TextField(default='')
+    match_master = models.ForeignKey('self', blank=True, null=True)
         
     def __unicode__(self):
         return self.name
